e84311e81b9c0d92c8e5d1e42c289ec4d7b271aa
jacqueshebert <jacques.henri.hebert@gmail.com>
2020-02-11 12:14:31 -0600

    menu restructured phase 1.5

--- bin/menu.rb
+++ bin/menu.rb
@@ -1,23 +1,30 @@
 require_relative '../config/environment'
 require 'tty-prompt'
 
+def startmenu()
+  start = TTY::Prompt.new
+  ans = start.select("Welcome to Day Planner", requied: true) do |menu|
+      menu.choice "Login", 1 #login_prompt()
+      menu.choice "Sign Up", 2 #signup_prompt()
+  end
 
-start = TTY::Prompt.new
-
-start.select("Welcome to Day Planner", requied: true) do |menu|
-    menu.choice "Login", self.login_prompt()
-    menu.choide "Sign Up",self.signup_prompt()
+  case ans
+  when 1
+    login_prompt()
+  when 2
+    signup_prompt()
+  end
 end
 
 def login_prompt()
-  login = TTY:Prompt.new()
+  login = TTY::Prompt.new()
   user = login.ask("Name: ", required: true)
   user  = User.all.find(user)
   main_menu(user.id)
 end
 
 def signup_prompt()
-  signup = TTY:Prompt.new()
+  signup = TTY::Prompt.new()
   user = signup.ask("Name: ", required: true)
   location = signup.ask("Enter Zip Code: ", required: true)
   contact = signup.ask("Enter Email: ", required: true)
@@ -34,18 +41,26 @@ def signup(name, location, contact)
 end
 
 
-def main_menu(user_id)
-  main_menu = TTY:Prompt.new()
-  main_menu.select("Tools: ", required: true) do |menu|
-    menu.choice "ADD", add_menu(user_id)
-    menu.choice "EDIT", edit_menu(user_id)
-    menu.choice "VIEW", view_menu(user_id)
-    menu.choice "DELTE", delete_menu(user_id)
+def user_menu(user_id)
+  user_menu = TTY::Prompt.new()
+  ans = user_menu.select("Tools: ", required: true) do |menu|
+    menu.choice "ADD", 1 #add_menu(user_id)
+    menu.choice "VIEW", 2 #view_menu(user_id)
+    menu.choice "DELTE", 3 #delete_menu(user_id)
+  end
+
+  case ans
+  when 1
+    add_menu(user_id)
+  when 2
+    view_menu(user_id)
+  when 3
+    delete_menu(user_id)
   end
 end
 
 def add_menu(user_id)
-  add_menu = TTY:Prompt.new()
+  add_menu = TTY::Prompt.new()
   add_menu.select("ADD: ", required: true) do |menu|
     menu.choice "Items", add_items(user_id)
     menu.choice "Events", add_events(user_id)
@@ -65,7 +80,7 @@ end
 # end
 
 def view_menu(user_id)
-  view_menu = TTY:Prompt.new()
+  view_menu = TTY::Prompt.new()
   view_menu.select("VIEW: ", required: true) do |menu|
     menu.choice "Items", view_items(user_id)
     menu.choice "Events", view_events(user_id)
@@ -75,7 +90,7 @@ def view_menu(user_id)
 end
 
 def delete_menu(user_id)
-  delete_menu = TTY:Prompt.new()
+  delete_menu = TTY::Prompt.new()
   delete_menu.select("DELETE: ", required: true) do |menu|
     menu.choice "Items", delete_items(user_id)
     menu.choice "Events", delete_events(user_id)
@@ -85,9 +100,11 @@ def delete_menu(user_id)
 end
 
 def add_items(user_id)
-  add_item = TTY:Prompt.new()
+  add_item = TTY::Prompt.new()
   add_item.select("Item Type: ", required: true) do |menu|
     menu.choice "Daily"
     menu.choice ""
   end
 end
+
+startmenu()
